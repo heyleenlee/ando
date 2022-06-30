7ac3328609098ebfc09ab4e1f4b4f623a36eab4f
李海林 <hailin.li@live.com>
2022-06-30 11:28:09 +0800

    修改数据量配置文件

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -68,11 +68,11 @@ WSGI_APPLICATION = 'wxcloudrun.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("MYSQL_DATABASE", 'django_demo'),
-        'USER': os.environ.get("MYSQL_USERNAME"),
-        'HOST': os.environ.get("MYSQL_ADDRESS").split(':')[0],
-        'PORT': os.environ.get("MYSQL_ADDRESS").split(':')[1],
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+        'NAME': 'heyleendasikong',
+        'USER': 'heyleendasikong',
+        'HOST': 'sql.wsfdb.cn',
+        'PORT': 3306,
+        'PASSWORD': 'Aa123456',
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
